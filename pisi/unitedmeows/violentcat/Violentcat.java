c6b9e04db03368269559d892c1e3c2cdd52e4fbc
Viserys <67641613+theviserys@users.noreply.github.com>
2022-01-13 17:03:36 +0300

    nbr
    nbr

--- pisi/unitedmeows/violentcat/Violentcat.java
+++ pisi/unitedmeows/violentcat/Violentcat.java
@@ -1,5 +1,21 @@
 package pisi.unitedmeows.violentcat;
 
+import pisi.unitedmeows.violentcat.headers.VHeader;
+import pisi.unitedmeows.yystal.web.YWebClient;
+
 public class Violentcat {
+
+    public static void main(String[] args) {
+        /* test here */
+
+        String token = "OTMxMTgwNDA3Njk5OTU5ODc4.YeArVQ.T6qvPT72qSx7k_Wqsn-U5sGfS1E";
+        YWebClient yWebClient = new YWebClient();
+        yWebClient.header(VHeader.AUTHORIZATION, VHeader.BOT + token);
+        yWebClient.setUserAgent("cats");
+        String result = yWebClient.downloadString("https://discord.com/api/v8/users/903746238447501322");
+        System.out.println(result);
+
+
+    }
 	
 }
---
+++ pisi/unitedmeows/violentcat/headers/VHeader.java
@@ -0,0 +1,9 @@
+package pisi.unitedmeows.violentcat.headers;
+
+public class VHeader {
+
+    /** the auth headers of discord */
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BOT = "Bot "; /*<token>*/
+
+}
